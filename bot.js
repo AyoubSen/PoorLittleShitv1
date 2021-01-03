9d9f6fa88dc18d455472b7578239e28ed0f12a9d
Ayoub Senhaji <senraja0000@gmail.com>
2021-01-03 20:45:46 +0100

    witcheryv.3

--- bot.js
+++ bot.js
@@ -1,127 +1,98 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.login(process.env.TOKEN);
 
-client.on('ready', readyDiscord);
+client.on("ready", readyDiscord);
 
-function readyDiscord(){
-
-    console.log('Bot is ready to be used');
-
+function readyDiscord() {
+  console.log("Bot is ready to be used");
 }
 
 let PREFIX = "@everyone";
 
-
 // This deletes any message where someone has included @everyone, and tells them to not do it.
-//------------------------------------------------------------------- 
-client.on('message', message => {
-	
-    
-    if ((message.content.includes(PREFIX)) /*&& (message.channel.id == '597149043793068053')*/){
-        message.delete();
-        message.channel.send("Don't tag everyone");
-	}
+//-------------------------------------------------------------------
+client.on("message", (message) => {
+  if (
+    message.content.includes(
+      PREFIX
+    ) /*&& (message.channel.id == '597149043793068053')*/
+  ) {
+    message.delete();
+    message.channel.send("Don't tag everyone");
+  }
 });
-//------------------------------------------------------------------- 
-
+//-------------------------------------------------------------------
 
 let lastActionId;
 
 // This is to know who was moved by whom
-//------------------------------------------------------------------- 
-client.on('voiceStateUpdate', async hh => {
+//-------------------------------------------------------------------
+client.on("voiceStateUpdate", async (hh) => {
+  let botroom = client.channels.cache.get("794960072702033980");
 
-    let botroom =  client.channels.cache.get('794960072702033980');
+  let nnhh = (
+    await client.guilds.cache
+      .get("212250736254255104")
+      .fetchAuditLogs({ limit: 1 })
+  ).entries.array()[0];
 
-    let nnhh = (await client.guilds.cache.get('212250736254255104').fetchAuditLogs({limit:1})).entries.array()[0];
-
-    
-    if((nnhh.action === "MEMBER_MOVE") && (nnhh.id !== lastActionId)){
-        lastActionId = nnhh.id;
-        let tmova = hh.member.user.username;
-        let mova = nnhh.executor.username;
-        if(tmova !== mova){
-            botroom.send(tmova + " was moved by " + mova );
-            console.log(nnhh);
-        }
-        
+  if (nnhh.action === "MEMBER_MOVE" && nnhh.id !== lastActionId) {
+    lastActionId = nnhh.id;
+    let tmova = hh.member.user.username;
+    let mova = nnhh.executor.username;
+    if (tmova !== mova) {
+      botroom.send(tmova + " was moved by " + mova);
+      console.log(nnhh);
     }
-}); 
-//------------------------------------------------------------------- 
-
-
-
+  }
+});
+//-------------------------------------------------------------------
 
 // this is to know who created an invite link
-//------------------------------------------------------------------- 
-client.on('inviteCreate', invcreate =>{
-
-    let botroom =  client.channels.cache.get('794960072702033980');
-    let whoDidIt = invcreate.inviter.username;
-    botroom.send("An invitation link was created by " + whoDidIt);
+//-------------------------------------------------------------------
+client.on("inviteCreate", (invcreate) => {
+  let botroom = client.channels.cache.get("794960072702033980");
+  let whoDidIt = invcreate.inviter.username;
+  botroom.send("An invitation link was created by " + whoDidIt);
 });
-//------------------------------------------------------------------- 
-
-
-
-
-
-
+//-------------------------------------------------------------------
 
 // this is to know whom message was deleted
-//------------------------------------------------------------------- 
-client.on('messageDelete', msgdeleted => {
+//-------------------------------------------------------------------
+client.on("messageDelete", async (msgdeleted) => {
+  let takeFromAudit = (
+    await client.guilds.cache
+      .get("212250736254255104")
+      .fetchAuditLogs({ limit: 1 })
+  ).entries.array()[0];
+  let botroom = client.channels.cache.get("794960072702033980");
+  let whosemessage = msgdeleted.author.username;
+  let text = msgdeleted.cleanContent;
 
-    let botroom =  client.channels.cache.get('794960072702033980');
-    let whosemessage = msgdeleted.author.username;
-    let text = msgdeleted.cleanContent;
+  if (takeFromAudit.action === "MESSAGE_DELETE") {
+    let whenWasIt = takeFromAudit.createdTimestamp;
+    let whoDidItId = takeFromAudit.executor.username;
+    let now = Date.now();
 
-    console.log(msgdeleted);
-    botroom.send("A message of " + whosemessage + " was deleted");
+    if (now - whenWasIt >= 1000) {
+      botroom.send(
+        "A message of " + whosemessage + " was deleted by " + whoDidItId
+      );
+      botroom.send("' " + text + " '");
+    } else {
+      botroom.send(whosemessage + "Deleted his own message");
+      botroom.send("' " + text + " '");
+    }
+  } else {
+    botroom.send(whosemessage + "Deleted his own message");
     botroom.send("' " + text + " '");
-    
-      
+  }
 });
 
-
-    
-    
-    
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// so i know how big of a failure i am: 
-
-
+// so i know how big of a failure i am:
 
 /*
 
